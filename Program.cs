aeac27d47fee8afa5ca72697c4fb5023cbc66ce4
Aleksey <rusal@bk.ru>
2022-09-17 10:17:23 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TMP_CS;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+Queue tmp = new Queue();
+tmp.Push(10);
+tmp.Push(20);
+tmp.Push(30);
+tmp.Push(40);
+
+tmp.Print();
+
+Console.WriteLine(tmp.Get(3));
+Console.WriteLine("---------");
+
+tmp.Print();
+
+Console.WriteLine("----------------------");
+
+tmp.Set(2, 5);
+tmp.Print();
+
+
+//Пузырьковая сортировка
+for (int i = 0; i < tmp.Count(); ++i)
+{
+    for (int j = i; j < tmp.Count() - 1; ++j)
+    {
+        if (tmp.Get(j) > tmp.Get(j + 1))
+        {
+            int tempValue = tmp.Get(j);
+            tmp.Set(j, tmp.Get(j + 1));
+            tmp.Set(j + 1, tempValue);
+        }
+    }
+}
+
+tmp.Print();
